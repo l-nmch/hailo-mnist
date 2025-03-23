7c93787476162f958b087a82cfb39cef62ac3c00
l-nmch <leo@nonnenmacher-logel.fr>
2025-03-23 03:33:51 +0100

    fix: removed unnecessary comments

--- src/calibration_set.py
+++ src/calibration_set.py
@@ -4,7 +4,6 @@ from PIL import Image, ImageOps
 
 import argparse
 
-# use argparse to get the image folder
 parser = argparse.ArgumentParser(description='Create a calibration set for the model.')
 parser.add_argument('--image_folder', type=str, required=True, help='Name of the folder containing the images')
 parser.add_argument('--output', type=str, required=True, help='Name of the output file .npy')
@@ -29,8 +28,8 @@ for i, img_path in enumerate(image_paths):
     img_inverted = ImageOps.invert(img_resized.convert("L"))
     img_array = np.array(img_inverted)
     img_array = img_array.astype(np.float32) / 255.0
-    img_array = np.expand_dims(img_array, axis=-1)  # (224, 224, 1)
-    img_array = np.repeat(img_array, img_channels, axis=-1)  # (224, 224, 3)
+    img_array = np.expand_dims(img_array, axis=-1)
+    img_array = np.repeat(img_array, img_channels, axis=-1)
     calib_set[i] = img_array
 
 np.save(args.output, calib_set)
